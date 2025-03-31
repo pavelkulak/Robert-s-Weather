b46ce121b91ec2b3501960fde54a000f20b8acd3
Robertimor <dybina0012@mail.ru>
2025-04-01 00:54:31 +0300

    Fix: Добавил API-ключи в vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/Robert-s-Weather/", // Замени на название твоего репозитория
+    define: {
+        'import.meta.env.VITE_API_KEY': JSON.stringify('4e88cbe360a5181d59fb73d2bee0c230'),
+        'import.meta.env.VITE_UNSPLASH_KEY': JSON.stringify('Ie-v99zRFc6xkpCVhSXQKKRdwzheohCdmwvcxcDScYw')
+    }
 });
