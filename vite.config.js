31ebbda45c88c0c009894f2be8ca301f0a37c048
Robertimor <dybina0012@mail.ru>
2025-05-22 10:01:15 +0300

    1) Заменил контейнер .control на <nav> 2) Убрал доп обёртку, что внутри nav 3) Добавил normalize 4) Реализовал спрайты для иконок 5) Переделал div'ы под температуры на buttnon 7) Убрал все лишние доп. секции (с классом container). (который сразу внутри section)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import ViteSvgSpriteWrapper from 'vite-svg-sprite-wrapper';
 
 export default defineConfig({
     base: "/Robert-s-Weather/", // Замени на название твоего репозитория
     define: {
         'import.meta.env.VITE_API_KEY': JSON.stringify('4e88cbe360a5181d59fb73d2bee0c230'),
         'import.meta.env.VITE_UNSPLASH_KEY': JSON.stringify('Ie-v99zRFc6xkpCVhSXQKKRdwzheohCdmwvcxcDScYw')
-    }
+    },
+    plugins: [
+		ViteSvgSpriteWrapper({
+			icons: './src/icon/*.svg',
+			outputDir: './public/'
+        })
+	]
 });
