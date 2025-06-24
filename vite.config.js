c3fe3964c73796f68ee6bfc763e6b8daf628c66a
Robertimor <dybina0012@mail.ru>
2025-06-24 20:54:41 +0300

    Проблемы с svg

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import ViteSvgSpriteWrapper from 'vite-svg-sprite-wrapper';
 
 export default defineConfig({
-    base: "/Robert-s-Weather/", // Замени на название твоего репозитория
+    base: "/Robert-s-Weather/",
     define: {
         'import.meta.env.VITE_API_KEY': JSON.stringify('4e88cbe360a5181d59fb73d2bee0c230'),
         'import.meta.env.VITE_UNSPLASH_KEY': JSON.stringify('Ie-v99zRFc6xkpCVhSXQKKRdwzheohCdmwvcxcDScYw')
