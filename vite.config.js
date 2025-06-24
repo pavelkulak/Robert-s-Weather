2b694426d1736d493e23446cd58388bbfc944aea
pavelkulak <kulak-pavel@yandex.ru>
2025-06-24 22:17:34 +0400

    Fix image paths for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import ViteSvgSpriteWrapper from 'vite-svg-sprite-wrapper';
 
 export default defineConfig({
-    base: "/Robert-s-Weather/",
     define: {
-        'import.meta.env.VITE_API_KEY': JSON.stringify('4e88cbe360a5181d59fb73d2bee0c230'),
-        'import.meta.env.VITE_UNSPLASH_KEY': JSON.stringify('Ie-v99zRFc6xkpCVhSXQKKRdwzheohCdmwvcxcDScYw')
+        'import.meta.env.VITE_API_KEY': JSON.stringify(
+            '4e88cbe360a5181d59fb73d2bee0c230'
+        ),
+        'import.meta.env.VITE_UNSPLASH_KEY': JSON.stringify(
+            'Ie-v99zRFc6xkpCVhSXQKKRdwzheohCdmwvcxcDScYw'
+        ),
     },
     plugins: [
-		ViteSvgSpriteWrapper({
-			icons: './src/icon/*.svg',
-			outputDir: './public/'
-        })
-	]
+        ViteSvgSpriteWrapper({
+            icons: './src/icon/*.svg',
+            outputDir: './public/',
+        }),
+    ],
 });
